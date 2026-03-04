fe9e1bf745a35f8c8e48de3edc47d5ef6c1c3b6e
SithagiPradeepkumara <153867463+SithagiPradeepkumara@users.noreply.github.com>
2026-03-04 10:56:15 +0530

    manual lab entry and grpah

--- mobile-app/src/api/axiosConfig.js
+++ mobile-app/src/api/axiosConfig.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 // Centralized IP Configuration
 // Change this ONE IP to update both Backend (5000) and Chatbot Server (8001)
-const API_IP = "192.168.8.128";
+const API_IP = "192.168.240.97";
 
 // Exported URLs for use across the app
 export const API_URL = `http://${API_IP}:5000/api`;
